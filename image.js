452c2d38b942e8fad05c89618d84b8f0175d58b5
Andrew Hill <contact@andrewthomashill.co.uk>
2022-04-08 12:45:51 +0200

    Fix transition on iOS

--- image.js
+++ image.js
@@ -1,10 +1,11 @@
+const parent = document.querySelector('.image');
 const images = document.querySelectorAll('.image img');
 
 let i = 1;
 window.setInterval(() => {
-	console.log(i)
 	images.forEach((image, index) => {
 		image.className = index === i ? 'is-active' : ''
 	})
+	parent.style.backgroundImage = `url(${images[i].src})`;
 	i = i < 2 ? i + 1 : 0;
 }, 4000);
